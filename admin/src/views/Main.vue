bff170f433631951b52f8f62b4d5061529a22395
xiedaokun <102251868@qq.com>
2021-04-21 19:51:41 +0800

    1

--- admin/src/views/Main.vue
+++ admin/src/views/Main.vue
@@ -11,7 +11,7 @@
           </el-menu-item-group>
           <el-menu-item-group>
             <template slot="title">分类</template>
-            <el-menu-item index="/categories/create">新建分类</el-menu-item>
+            <el-menu-item index="/categories/create">新建分类1</el-menu-item>
             <el-menu-item index="/categories/list">分类列表</el-menu-item>
           </el-menu-item-group>
         </el-submenu>
