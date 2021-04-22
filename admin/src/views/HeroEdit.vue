874dd1a74d9c6ac641cc4b7a493afd027786f1cd
xiedaokun <102251868@qq.com>
2021-04-22 13:43:57 +0800

    xdk

--- admin/src/views/HeroEdit.vue
+++ admin/src/views/HeroEdit.vue
@@ -2,6 +2,17 @@
   <div class="about">
     <h1>{{ id ? '编辑' : '新增' }}英雄</h1>
     <el-form label-width="120px" @submit.native.prevent="save">
+      <el-form-item label="头像">
+        <el-upload
+            class="avatar-uploader"
+            :action="$http.defaults.baseURL+'/upload'"
+            :show-file-list="false"
+            :on-success="afterUpload"
+        >
+          <img v-if="model.avatar" :src="model.avatar" class="avatar">
+          <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+        </el-upload>
+      </el-form-item>
       <el-form-item label="名称">
         <el-input v-model="model.name"></el-input>
       </el-form-item>
@@ -9,24 +20,38 @@
         <el-input v-model="model.title"></el-input>
       </el-form-item>
       <el-form-item label="类型">
-        <el-select v-model="model.categories">
+        <el-select v-model="model.categories" multiple>
           <el-option
               v-for="item in categories"
               :key="item._id"
               :label="item.name"
-              :value="item._id">
+              :value="item._id"
+          >
           </el-option>
         </el-select>
       </el-form-item>
-      <el-form-item label="头像">
-        <el-upload
-            class="avatar-uploader"
-            :action="$http.defaults.baseURL+'/upload'"
-            :show-file-list="false"
-            :on-success="afterUpload">
-          <img v-if="model.avatar" :src="model.avatar" class="avatar">
-          <i v-else class="el-icon-plus avatar-uploader-icon"></i>
-        </el-upload>
+      <el-form-item label="难度">
+        <el-rate v-model="model.scores.difficult" style="margin-top: 0.6rem" :max="9" show-score></el-rate>
+      </el-form-item>
+      <el-form-item label="技能">
+        <el-rate v-model="model.scores.skills" style="margin-top: 0.6rem" :max="9" show-score></el-rate>
+      </el-form-item>
+      <el-form-item label="攻击">
+        <el-rate v-model="model.scores.attack" style="margin-top: 0.6rem" :max="9" show-score></el-rate>
+      </el-form-item>
+      <el-form-item label="生存">
+        <el-rate v-model="model.scores.survive" style="margin-top: 0.6rem" :max="9" show-score></el-rate>
+      </el-form-item>
+      <el-form-item label="顺风出装">
+        <el-select v-model="model.items1" multiple>
+          <el-option
+              v-for="item in items"
+              :key="item._id"
+              :label="item.name"
+              :value="item._id"
+          >
+          </el-option>
+        </el-select>
       </el-form-item>
       <el-form-item>
         <el-button type="primary" native-type="submit">保存</el-button>
@@ -43,9 +68,14 @@ export default {
     return {
       model: {
         name: '',
-        avatar: ''
+        avatar: '',
+        categories: [],
+        scores: {
+          difficult:0
+        }
       },
-      categories: []
+      categories: [],
+      items: []
     }
   },
   methods: {
@@ -66,15 +96,20 @@ export default {
     },
     async fetch() {
       const res = await this.$http.get(`rest/heroes/${this.id}`)
-      this.model = res.data
+      this.model = Object.assign({}, this.model, res.data)
     },
     async fetchCategories() {
       const res = await this.$http.get(`rest/categories`)
       this.categories = res.data
+    },
+    async fetchItems() {
+      const res = await this.$http.get(`rest/items`)
+      this.items = res.data
     }
   },
   created() {
     this.fetchCategories()
+    this.fetchItems()
     this.id && this.fetch()
   }
 }
